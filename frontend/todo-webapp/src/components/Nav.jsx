97348b4dd9413b79dea0aff4c99f58f03073851d
Anand <73114463+Anand4756@users.noreply.github.com>
2021-10-10 22:02:38 +0530

    contextapi

--- frontend/todo-webapp/src/components/Nav.jsx
+++ frontend/todo-webapp/src/components/Nav.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Link } from 'react-router-dom'
+import { CredentialContext } from '../App'
 
 const Nav = () => {
+    const [nameoflogged] = useContext(CredentialContext)
     return (
         <div className="navbar">
             <h1>TODO</h1>
-            <Link to="/register"><button>LOGIN/REGISTER</button></Link>
+            {!nameoflogged && <Link to="/register"><button>LOGIN/REGISTER</button></Link>}
+            {nameoflogged && nameoflogged.name}
         </div>
     )
 }
